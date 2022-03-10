1559c6dc5ebcd1c54fea3a1bb58a9bb6512e65d7
Vasileios Dimitriou <dimitriouavasileios@gmail.com>
2022-03-10 18:13:50 +0200

    0x76 - 0x77
    this code does interchangably tweak of the slave address, experimenting myself. Uploaded also Fritzing file

--- BME280.ino
+++ BME280.ino
@@ -1,43 +1,48 @@
-
 #include <Wire.h>
 #include <Adafruit_BME280.h>
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+#define SEALEVELPRESSURE_HPA (1022.2)
+int pin_num = 7;     //pin that controls the slave address
+
 
 Adafruit_BME280 bme;
 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(pin_num, OUTPUT);  //we use this pin in order to control the slave address
+  digitalWrite(pin_num,LOW); //we start with low at the pin 7 corresponding to GND at the SD0 pin
   if (!bme.begin(0x76)) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
+    //while (1);
   }
 }
 
 void loop() {
-  Serial.print("Temperature = ");
+
+  bme.begin(0x76);         // here we define the first address 0x76
+  digitalWrite(pin_num,LOW);
+  
+  delay(1000);
   Serial.print(bme.readTemperature());
-  Serial.println("*C");
-
-  Serial.print("Pressure = ");
-  Serial.print(bme.readPressure() / 100.0F);
-  Serial.println("hPa");
-
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println("m");
-
-  Serial.print("Humidity = ");
-  Serial.print(bme.readHumidity());
-  Serial.println("%");
-
-  Serial.print(bme.readTemperature());
-  Serial.print(" , ");
+  Serial.print(" ");
   Serial.print(bme.readPressure() / 100.0F );
-  Serial.print(" , ");
-  Serial.println(bme.readHumidity());
-  Serial.println("-----------");
+  Serial.print(" ");
+  Serial.print(bme.readHumidity());
+  Serial.print(" ");
+  Serial.println(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  delay(1000);
+  
+  bme.begin(0x77);         // here we define the second address 0x77
+  digitalWrite(pin_num,HIGH);
+  
+  delay(1000); 
+  Serial.print(bme.readTemperature());
+  Serial.print(" ");
+  Serial.print(bme.readPressure() / 100.0F );
+  Serial.print(" ");
+  Serial.print(bme.readHumidity());
+  Serial.print(" ");
+  Serial.println(bme.readAltitude(SEALEVELPRESSURE_HPA));
 
   delay(5000);
 }
